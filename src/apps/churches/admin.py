c57ac1902e2980581785fc7894c6b51c0f8236c1
Gilson Filho <contato@gilsondev.com>
2012-09-20 21:01:02 -0300

    Corrigindo register do admin

--- src/apps/churches/admin.py
+++ src/apps/churches/admin.py
@@ -2,10 +2,10 @@
 
 from django.contrib import admin
 
-from churches.models import Church
+from src.apps.churches.models import Church
 
 
 class ChurchAdmin(admin.ModelAdmin):
     list_display = ('name', 'site', 'email', 'phone', 'members')
 
-admin.site.register(ChurchAdmin, Church)
+admin.site.register(Church, ChurchAdmin)
